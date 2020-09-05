6967f68c4d1ce591caf5adf060e721ba82ce667c
shimehituzi <shimehituzi@gmail.com>
2020-09-06 05:28:59 +0900

    mui theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { createMuiTheme, colors, ThemeProvider, CssBaseline } from '@material-ui/core'
 import { store } from 'state'
 import App from 'views/App'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: colors.lightBlue,
+    secondary: colors.yellow,
+    error: colors.red,
+    background: {
+      default: colors.blueGrey.A700
+    }
+  },
+})
+
 render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <CssBaseline/>
+      <App/>
+    </Provider>
+  </ThemeProvider>
   , document.getElementById('root')
 )
