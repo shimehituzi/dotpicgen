122eb2afdfc0a73627969d10692d2546c3b35c64
shimehituzi <shimehituzi@gmail.com>
2020-09-06 11:30:24 +0900

    deleted test

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from 'views/Home'
-import Test from 'views/Test'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Route exact path="/"><Home/></Route>
-      <Route path="/test"><Test/></Route>
     </BrowserRouter>
   )
 }
