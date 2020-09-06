9086ec909f0f597befa4145f0e16326df4d076fb
shimehituzi <shimehituzi@gmail.com>
2020-09-06 16:00:20 +0900

    Updates

---
+++ precache-manifest.cd2dda25a8954324a3542818b80bad6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae578dcde3e68a81b3f43c890196faf",
+    "url": "/dotpicgen/index.html"
+  },
+  {
+    "revision": "6edbb1f67977cb7ea3b3",
+    "url": "/dotpicgen/static/js/2.229b3705.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dotpicgen/static/js/2.229b3705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ab2775df85ceea14f9",
+    "url": "/dotpicgen/static/js/main.1655230e.chunk.js"
+  },
+  {
+    "revision": "9d5a104f9ddddf1e6c38",
+    "url": "/dotpicgen/static/js/runtime-main.e4147262.js"
+  }
+]);
