ff3e00ee0d11bec719c1fdcdebed87081158fc4a
shimehituzi <shimehituzi@gmail.com>
2020-09-06 11:56:57 +0900

    loop

--- src/views/ColorPullet/index.tsx
+++ src/views/ColorPullet/index.tsx
@@ -12,18 +12,20 @@ const ColorPullet: React.FC = () => {
   const changeColor = useCallback(
     (_:React.MouseEvent<HTMLElement, MouseEvent>, color: Colors) => {
       dispatch(actions.color.setColor(color))
-      console.log("clicked")
     }, [dispatch]
   )
 
   return (
     <ToggleButtonGroup size="small" value={color.pulletColor} exclusive onChange={changeColor}>
-      <ToggleButton value={Colors.Gray}><Brightness1 style={{color: Colors.Gray}}/></ToggleButton>
-      <ToggleButton value={Colors.Red}><Brightness1 style={{color: Colors.Red}}/></ToggleButton>
-      <ToggleButton value={Colors.Blue}><Brightness1 style={{color: Colors.Blue}}/></ToggleButton>
-      <ToggleButton value={Colors.Green}><Brightness1 style={{color: Colors.Green}}/></ToggleButton>
-      <ToggleButton value={Colors.Yellow}><Brightness1 style={{color: Colors.Yellow}}/></ToggleButton>
-      <ToggleButton value={Colors.White}><Brightness1 style={{color: Colors.White}}/></ToggleButton>
+      {
+        Object.entries(Colors).map(([_, color], index) => {
+          return (
+            <ToggleButton key={index} value={color}>
+              <Brightness1 style={{color: color}}/>
+            </ToggleButton>
+          )
+        })
+      }
     </ToggleButtonGroup>
   )
 }
